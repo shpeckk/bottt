e09810c922963fd54b0a75d049b069d17105b357
shpeckk <migulev903@gmail.com>
2025-07-13 22:21:50 +0700

    init

---
+++ main.py
@@ -0,0 +1,27 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.utils import executor
+
+API_TOKEN = 'YOUR_BOT_TOKEN'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(KeyboardButton("Заказать кальян"))
+    keyboard.add(KeyboardButton("Меню и цены"))
+    await message.answer("Привет! Выбери действие:", reply_markup=keyboard)
+
+@dp.message_handler(lambda message: message.text == "Заказать кальян")
+async def hookah_order(message: types.Message):
+    await message.answer("Введите вкус, крепость (1-5) и номер телефона.")
+
+@dp.message_handler(lambda message: message.text == "Меню и цены")
+async def menu(message: types.Message):
+    await message.answer("Вот наше меню и цены (тут будет фото или текст).")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
